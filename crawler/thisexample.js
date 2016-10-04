87f74314c791f1404e702b1166329f00bcb5ac9d
lii5a <liisa.tallinn@itcollege.ee>
2016-10-04 15:03:21 +0300

    more games with this

--- crawler/thisexample.js
+++ crawler/thisexample.js
@@ -3,7 +3,39 @@ var Bucky = {
 	console.log("My name is Bucky");
 	console.log(this === Bucky);
 	}
-
 };
 
+
+//why function inside variable? 
+//aha. the function is a parameter of this variable.
+
+
 Bucky.printFirstName();
+
+//default calling context is global
+function doSomethingWorthless() {
+	console.log("\n I am worthless");
+	console.log(this === global);
+}
+
+doSomethingWorthless();
+
+function User () {
+	this.name = "";
+	this.life = 100;
+	this.giveLife = function giveLife(targetPlayer) {
+		targetPlayer.life += 1;
+		this.life -= 1;
+		console.log(this.name + " gave 1 life to " + targetPlayer.name);
+	}
+
+}
+
+var Bucky = new User();
+var Wendy = new User();
+
+Bucky.name = "Bucky";
+Wendy.name = "Wendy";
+Bucky.giveLife(Wendy);
+console.log("Bucky " + Bucky.life);
+console.log("Wendy " + Wendy.life);
